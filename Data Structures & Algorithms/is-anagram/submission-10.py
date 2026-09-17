746c9ad640898b09c8372f0f8e6d6bcdc937f0df
Carter71 <132308606+Carter71@users.noreply.github.com>
2026-09-17 12:00:52 -0400

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Or just use this here
+        if Counter(s) == Counter(t):
+            return True
+        return False
+        # hashs, hasht = {}, {}
+        # if len(s) != len(t):
+        #     return False
+        # for i in range(len(s)):
+        #     hashs[s[i]] = 1 + hashs.get(s[i], 0) # the get allow us to that 
+        #     hasht[t[i]] = 1 + hasht.get(t[i], 0) # same value as the variable
+        # for c in hashs:
+        #     if hashs[c] != hasht.get(c, 0):
+        #         return False
+        # return True
+        # The goal is to iterate through each word and record the counts of each letter in a separt dictionary, then compare each key value pair to the other dictionary
